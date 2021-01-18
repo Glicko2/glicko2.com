0965816a9cff25f0c9516a7a0582f129c1c81752
Matthew Hatcher <matthewh@halfpetal.com>
2021-01-17 20:59:35 -0500

    Adding some other stuff.

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
     <nav
-      class="navbar header has-shadow is-primary"
+      class="navbar header has-shadow has-background-dark"
       role="navigation"
       aria-label="main navigation"
     >
       <div class="navbar-brand">
         <a class="navbar-item" href="/">
-          <img src="~assets/buefy.png" alt="Buefy" height="28" />
+            <h1 class="title has-text-white">Title</h1>
         </a>
 
         <div class="navbar-burger">
--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'glicko2.com',
+    title: 'Glicko2',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -17,7 +17,9 @@ export default {
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: [],
+  css: [
+      '@/assets/css/global.css',
+  ],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [],
