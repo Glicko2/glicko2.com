f8ba0db46f73d8dd442fe1d05a42f6b8ea7a0382
Matthew Hatcher <matthewh@halfpetal.com>
2021-01-20 11:51:24 -0500

    Changing slot location,

--- components/Card.vue
+++ components/Card.vue
@@ -8,16 +8,11 @@
       </header>
       <div class="card-content">
         <div class="content has-text-centered">
-          <b-icon :icon="icon" size="is-large" type="is-primary" />
-        </div>
-      </div>
-      <footer class="card-footer">
-        <div class="card-footer-item">
           <span>
             <slot />
           </span>
         </div>
-      </footer>
+      </div>
     </div>
   </div>
 </template>
